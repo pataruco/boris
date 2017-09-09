4e6f06f744ca640e7a5711462d4781b07641475c
Pedro Martin <pataruco@gmail.com>
2017-09-09 22:05:20 +0100

    //Backup

--- scrapper.js
+++ scrapper.js
@@ -1,29 +1,29 @@
 const puppeteer = require('puppeteer');
-const MPIndexURL = 'http://www.parliament.uk/mps-lords-and-offices/mps/';
-const MPIndexSelector = 'table > tbody > tr > td > a';
-
 
 class ScrapeIndex {
-    constructor( url, selector ) {
-        this.url = url;
-        this.selector = selector;
-        return this.scrape( );
+    constructor(  ) {
+        this.url = 'http://www.parliament.uk/mps-lords-and-offices/mps/';
+        return this.scrape(  );
     }
 
-    async scrape( ) {
-        let selector = this.selector;
+    async scrape(   ) {
         let browser = await puppeteer.launch();
         let page = await browser.newPage();
-        await page.goto(this.url);
-        let list = await page.evaluate( ( ) =>{
-            return document.querySelectorAll( 'table > tbody > tr > td > a' )
+        await page.goto( this.url  );
+        const links = await page.evaluate( ( ) => {
+            const anchors = Array.from( document.querySelectorAll( 'table > tbody > tr > td > a' ) );
+            return anchors.map( ( anchor ) => {
+                let isNotMP = anchor.includes('#top');
+                if ( !isNotMP ) {
+                    return anchor.href;
+                }
+            });
+            // return anchors.map( anchor => anchor.href);
         });
-        console.log( this.selector );
-        console.log( list );
-
+        console.log(links);
         browser.close();
     }
 
 }
 
-let urlToScrape = new ScrapeIndex( MPIndexURL,  MPIndexSelector)
+let urlToScrape = new ScrapeIndex( )
