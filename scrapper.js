6c642d421b5ea273edd0a2105c9175e98dee5feb
Pedro Martin <pataruco@gmail.com>
2017-09-10 21:51:34 +0100

    Set MPs scrapper

--- scrapper.js
+++ scrapper.js
@@ -4,28 +4,19 @@ const MP = require('./src/mp.js');
 
 const index = new Index( );
 
-// console.log( index );
-// console.log(  index.scrape( ).then( () => console.log( 'links' , index.links )) )
-
-
 index.scrape( ).then( ( ) => {
-    getMPs( index.links );
+    const MPs = getMPs( index.links );
 });
 
 async function getMPs( links ) {
+    const mps = [ ];
     for ( link of links ) {
         if ( link ) {
-            console.log( link );
-            const mp =  await new MP( link );
-            mp.then( ( ) => {
-                console.log( 'here ');
-            })
-            // mp.then( ( ) => {
-            //     console.log( mpData );
-            // })
-            // mp.then( ( ) => {
-            //     console.log( mp.data );
-            // })
+            const mp =  new MP( link );
+            await mp.scrape( ).then( ( ) => {
+                mps.push( mp );
+            });
         }
     }
+    return mps;
 }
--- src/mp.js
+++ src/mp.js
@@ -3,23 +3,37 @@ const puppeteer = require('puppeteer');
 class MP {
     constructor( url  ) {
         this.url = url;
-        this.data = ' ';
-        return this.scrape(  );
+        this.data = { };
     }
 
     async scrape(   ) {
         let browser = await puppeteer.launch();
         let page = await browser.newPage();
         await page.goto( this.url );
-        await page.evaluate( ( ) => {
-            this.data = document.querySelector('h1').innerText;
-            // console.log( this.data );
-            // const anchors = Array.from( document.querySelectorAll( 'table > tbody > tr > td > a' ) );
-            // return anchors.map( ( anchor ) => {
-            //     if ( !anchor.href.includes('#') ) {
-            //         return anchor.href;
-            //     }
-            // });
+        this.data = await page.evaluate( ( ) => {
+            function getQuerySelector( query, attr ) {
+                let $element = document.querySelector(query);
+                if ( $element ) {
+                    if ( attr === 'text' ) {
+                        return $element.innerText;
+                    } else {
+                        return $element.href;
+                    }
+                }
+                return null;
+            }
+            return {
+                name: getQuerySelector('h1', 'text'),
+                constituency: getQuerySelector('#commons-constituency', 'text'),
+                addressAs: getQuerySelector('#commons-addressas', 'text'),
+                party: getQuerySelector('#commons-party', 'text'),
+                email: getQuerySelector('p[data-generic-id="email-address"] > a', 'text'),
+                twitter: {
+                    handler: getQuerySelector('li[data-generic-id="twitter"] > a', 'text'),
+                    url: getQuerySelector('li[data-generic-id="twitter"] > a', 'link')
+                },
+                website: getQuerySelector('li[data-generic-id="website"] > a', 'link')
+            }
         });
         browser.close();
     }
