593948a08d682fc5bf2a0680d50ee40b8021f3b9
Pedro Martin <pataruco@gmail.com>
2017-09-10 14:30:19 +0100

    Set an MP URL index object

--- scrapper.js
+++ scrapper.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-class ScrapeIndex {
+class MPindex {
     constructor(  ) {
         this.url = 'http://www.parliament.uk/mps-lords-and-offices/mps/';
         return this.scrape(  );
@@ -9,21 +9,17 @@ class ScrapeIndex {
     async scrape(   ) {
         let browser = await puppeteer.launch();
         let page = await browser.newPage();
-        await page.goto( this.url  );
+        await page.goto( this.url );
         const links = await page.evaluate( ( ) => {
             const anchors = Array.from( document.querySelectorAll( 'table > tbody > tr > td > a' ) );
             return anchors.map( ( anchor ) => {
-                let isNotMP = anchor.includes('#top');
-                if ( !isNotMP ) {
+                if ( anchor.text !== 'back to top') {
                     return anchor.href;
                 }
             });
-            // return anchors.map( anchor => anchor.href);
         });
-        console.log(links);
         browser.close();
     }
-
 }
 
-let urlToScrape = new ScrapeIndex( )
+let urlToScrape = new MPindex( );
