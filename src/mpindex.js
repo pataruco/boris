a075ed0d406ad44703e3755c9f1fb98e3d2c0efb
Pedro Martin <pataruco@gmail.com>
2017-09-10 15:17:39 +0100

    //WIP

---
+++ src/mpindex.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+class MPindex {
+    constructor(  ) {
+        this.url = 'http://www.parliament.uk/mps-lords-and-offices/mps/';
+        return this.scrape(  );
+    }
+
+    async scrape(   ) {
+        let browser = await puppeteer.launch();
+        let page = await browser.newPage();
+        await page.goto( this.url );
+        const links = await page.evaluate( ( ) => {
+            const anchors = Array.from( document.querySelectorAll( 'table > tbody > tr > td > a' ) );
+            return anchors.map( ( anchor ) => {
+                if ( !anchor.href.includes('#') ) {
+                    return anchor.href;
+                }
+            });
+        });
+        browser.close();
+    }
+}
+
+module.exports = MPindex;
