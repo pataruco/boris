c9d57c720347c76d92bd955543ed160552db173c
Pedro Martin <pataruco@gmail.com>
2017-09-12 16:38:17 +0100

    Resolve promises and save MPs data into JSON

--- scrapper.js
+++ scrapper.js
@@ -1,12 +1,28 @@
 const Index = require('./src/index.js');
 const MP = require('./src/mp.js');
+const filesystem = require('fs');
+let numberOfMPs = 0;
+let numberofMPsScraped = 1;
 
 
-const index = new Index( );
+async function start( ) {
+    const index = new Index( );
+    const MPArray = await index.scrape( ).then( ( ) => {
+        const MPlinks = getMPlinks( index.links );
+        numberOfMPs = MPlinks.length;
+        return getMPs( index.links );
+    });
+    const MPJson = json.parse( MPArray );
+    saveMembersInAFile( MPJson );
+}
 
-index.scrape( ).then( ( ) => {
-    const MPs = getMPs( index.links );
-});
+function getMPlinks( array ) {
+    return array.filter( ( item ) => {
+        if ( item ) {
+            return item;
+        }
+    });
+}
 
 async function getMPs( links ) {
     const mps = [ ];
@@ -14,9 +30,21 @@ async function getMPs( links ) {
         if ( link ) {
             const mp =  new MP( link );
             await mp.scrape( ).then( ( ) => {
-                mps.push( mp );
+                console.log( `${ mp.data.name } has been scraped / ${numberofMPsScraped++} of ${numberOfMPs}` );
+                mps.push( mp.data );
             });
         }
     }
     return mps;
 }
+
+function saveMembersInAFile( json ) {
+    filesystem.writeFile('./data/members.json', json, ( error ) => {
+        if ( error ) {
+            return console.error( error );
+        }
+        console.log("The file was saved!");
+    })
+}
+
+start( );
