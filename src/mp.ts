e00cf986e4fbd7061d036d98afdd364bab7b4cb6
Pedro Martin <pataruco@gmail.com>
2019-02-17 18:10:51 +0000

    MP work on progress

--- src/mp.ts
+++ src/mp.ts
@@ -1,16 +1,27 @@
 import puppeteer from 'puppeteer';
 
-const getQuerySelector = (query: string, attr: string): string | null => {
-  const $element: HTMLAnchorElement | null = document.querySelector(query);
-  if ($element) {
+function getQuerySelector(query: string, attr: string): string | null {
+  const element: HTMLAnchorElement | null = document.querySelector(query);
+  if (element) {
     if (attr === 'text') {
-      return $element.innerText;
+      return element.innerText;
     } else {
-      return $element.href;
+      return element.href;
     }
   }
   return null;
-};
+}
+// const getQuerySelector = (query: string, attr: string): string | null => {
+//   const element: HTMLAnchorElement | null = document.querySelector(query);
+//   if (element) {
+//     if (attr === 'text') {
+//       return element.innerText;
+//     } else {
+//       return element.href;
+//     }
+//   }
+//   return null;
+// };
 
 export interface MP {
   name: string | null;
@@ -29,22 +40,24 @@ const scrape = async (url: string): Promise<MP> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const mp: MP = await page.evaluate(() => {
-    return {
-      name: getQuerySelector('h1', 'text'),
-      constituency: getQuerySelector('#commons-constituency', 'text'),
-      addressAs: getQuerySelector('#commons-addressas', 'text'),
-      party: getQuerySelector('#commons-party', 'text'),
-      email: getQuerySelector('p[data-generic-id="email-address"] > a', 'text'),
-      twitter: {
-        handler: getQuerySelector('li[data-generic-id="twitter"] > a', 'text'),
-        url: getQuerySelector('li[data-generic-id="twitter"] > a', 'link'),
-      },
-      website: getQuerySelector('li[data-generic-id="website"] > a', 'link'),
-    };
-  });
+  const mp: MP = await page.evaluate(() => ({
+    name: getQuerySelector('h1', 'text'),
+    constituency: getQuerySelector('#commons-constituency', 'text'),
+    addressAs: getQuerySelector('#commons-addressas', 'text'),
+    party: getQuerySelector('#commons-party', 'text'),
+    email: getQuerySelector('p[data-generic-id="email-address"] > a', 'text'),
+    twitter: {
+      handler: getQuerySelector('li[data-generic-id="twitter"] > a', 'text'),
+      url: getQuerySelector('li[data-generic-id="twitter"] > a', 'link'),
+    },
+    website: getQuerySelector('li[data-generic-id="website"] > a', 'link'),
+  }));
   browser.close();
   return mp;
 };
 
 export default scrape;
+
+scrape('https://www.parliament.uk/biographies/commons/greg-clark/1578').then(
+  data => console.log(data),
+);
