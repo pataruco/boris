15976a9ea89806400cc1d57c95e58a21797bf87a
Pedro Martin <pataruco@gmail.com>
2019-02-17 20:12:52 +0000

    Rename methods

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 const url = 'http://www.parliament.uk/mps-lords-and-offices/mps/';
 
-const scrape = async (): Promise<string[]> => {
+const getMpIndex = async (): Promise<string[]> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
@@ -17,4 +17,4 @@ const scrape = async (): Promise<string[]> => {
   return links;
 };
 
-export default scrape;
+export default getMpIndex;
--- src/mp.ts
+++ src/mp.ts
@@ -1,28 +1,5 @@
 import puppeteer from 'puppeteer';
 
-function getQuerySelector(query: string, attr: string): string | null {
-  const element: HTMLAnchorElement | null = document.querySelector(query);
-  if (element) {
-    if (attr === 'text') {
-      return element.innerText;
-    } else {
-      return element.href;
-    }
-  }
-  return null;
-}
-// const getQuerySelector = (query: string, attr: string): string | null => {
-//   const element: HTMLAnchorElement | null = document.querySelector(query);
-//   if (element) {
-//     if (attr === 'text') {
-//       return element.innerText;
-//     } else {
-//       return element.href;
-//     }
-//   }
-//   return null;
-// };
-
 export interface MP {
   name: string | null;
   constituency: string | null;
@@ -36,40 +13,43 @@ export interface MP {
   website: string | null;
 }
 
-const scrape = async (url: string): Promise<MP> => {
+const getMp = async (url: string): Promise<MP> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
   const mp: MP = await page.evaluate(() => ({
-    name: (document.querySelector('h1') as HTMLHeadingElement).innerText,
-    constituency: (document.querySelector(
-      '#commons-constituency',
-    ) as HTMLDivElement).innerText,
-    addressAs: (document.querySelector('#commons-addressas') as HTMLDivElement)
-      .innerText,
-    party: (document.querySelector('#commons-party') as HTMLDivElement)
-      .innerText,
-    email: (document.querySelector(
-      'p[data-generic-id="email-address"] > a',
-    ) as HTMLAnchorElement).innerText,
+    name:
+      (document.querySelector('h1') as HTMLHeadingElement).innerText || null,
+    constituency:
+      (document.querySelector('#commons-constituency') as HTMLDivElement)
+        .innerText || null,
+    addressAs:
+      (document.querySelector('#commons-addressas') as HTMLDivElement)
+        .innerText || null,
+    party:
+      (document.querySelector('#commons-party') as HTMLDivElement).innerText ||
+      null,
+    email:
+      (document.querySelector(
+        'p[data-generic-id="email-address"] > a',
+      ) as HTMLAnchorElement).innerText || null,
     twitter: {
-      handler: (document.querySelector(
-        'li[data-generic-id="twitter"] > a',
-      ) as HTMLAnchorElement).innerText,
-      url: (document.querySelector(
-        'li[data-generic-id="twitter"] > a',
-      ) as HTMLAnchorElement).href,
+      handler:
+        (document.querySelector(
+          'li[data-generic-id="twitter"] > a',
+        ) as HTMLAnchorElement).innerText || null,
+      url:
+        (document.querySelector(
+          'li[data-generic-id="twitter"] > a',
+        ) as HTMLAnchorElement).href || null,
     },
-    website: (document.querySelector(
-      'li[data-generic-id="website"] > a',
-    ) as HTMLAnchorElement).href,
+    website:
+      (document.querySelector(
+        'li[data-generic-id="website"] > a',
+      ) as HTMLAnchorElement).href || null,
   }));
   browser.close();
   return mp;
 };
 
-export default scrape;
-
-scrape('https://www.parliament.uk/biographies/commons/greg-clark/1578').then(
-  data => console.log(data),
-);
+export default getMp;
