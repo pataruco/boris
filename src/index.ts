924aa43ec73cef78fb5000f0fbda3ca9cd7b712d
Pedro Martin <pataruco@gmail.com>
2019-02-17 18:02:11 +0000

    Return an array of mp

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import puppeteer from 'puppeteer';
 
 const url = 'http://www.parliament.uk/mps-lords-and-offices/mps/';
 
-const scrape = async (): Promise<HTMLAnchorElement[]> => {
+const scrape = async (): Promise<string[]> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
   const links = await page.evaluate(() =>
     Array.from(document.querySelectorAll(
       'table > tbody > tr > td > a',
-    ) as NodeListOf<HTMLAnchorElement>).filter(
-      anchor => !anchor.href.includes('#'),
-    ),
+    ) as NodeListOf<HTMLAnchorElement>)
+      .filter(anchor => !anchor.href.includes('#'))
+      .map(anchor => anchor.href),
   );
   browser.close();
   return links;
