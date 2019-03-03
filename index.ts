f89d31982b1d9b5fc9047b69555ce2b3b1793c02
Pedro Martin <pataruco@gmail.com>
2019-03-03 12:23:24 +0000

    Guard selectors

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import getMpIndex from './src/index';
-import getMp from './src/mp.js';
-import fs from 'fs';
+import getMp, { MP } from './src/mp';
+// import fs from 'fs';
 import colors from 'colors/safe';
 
 let numberOfMPs = 0;
@@ -8,51 +8,78 @@ let numberofMPsScraped = 1;
 
 const start = async (): Promise<void> => {
   console.log(colors.bgWhite('Scraper started'));
-  const index = await getMpIndex();
-  const MPArray = await index.scrape().then(() => {
-    const MPlinks = getMPlinks(index.links);
-    numberOfMPs = MPlinks.length;
-    return getMPs(index.links);
-  });
-  const MPJson = JSON.stringify(MPArray);
-  saveMembersInAFile(MPJson);
+
+  let mpIndex;
+
+  try {
+    mpIndex = await getMpIndex();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+  numberOfMPs = mpIndex.length;
+  const mps = scrapeMps(mpIndex);
+  console.log(mps);
 };
 
-function getMPlinks(array) {
-  return array.filter(item => {
-    if (item) {
-      return item;
-    }
-  });
-}
+const scrapeMps = async (links: string[]): Promise<MP[]> => {
+  const mps: MP[] = [];
 
-async function getMPs(links) {
-  const mps = [];
-  for (link of links) {
-    if (link) {
-      const mp = new MP(link);
-      await mp.scrape().then(() => {
-        console.log(
-          colors.green(`${mp.data.name} `) +
-            `/ has been scrapped ` +
-            colors.yellow(`${numberofMPsScraped++} `) +
-            `of ` +
-            colors.green(`${numberOfMPs}`),
-        );
-        mps.push(mp.data);
-      });
+  links.forEach(async (link: string) => {
+    let mp: MP;
+    try {
+      mp = await getMp(link);
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-  }
+    console.log(
+      colors.green(`${mp.name} `) +
+        `/ has been scrapped ` +
+        colors.yellow(`${numberofMPsScraped++} `) +
+        `of ` +
+        colors.green(`${numberOfMPs}`),
+    );
+    mps.push(mp);
+  });
   return mps;
-}
+};
 
-function saveMembersInAFile(json) {
-  fs.writeFile('./data/members.json', json, error => {
-    if (error) {
-      return console.error(colors.red(JSON.stringify(error)));
-    }
-    console.log(colors.bgWhite('The file was saved!'));
-  });
-}
+// function getMPlinks(array) {
+//   return array.filter(item => {
+//     if (item) {
+//       return item;
+//     }
+//   });
+// }
+
+// async function getMPs(links) {
+//   const mps = [];
+//   for (link of links) {
+//     if (link) {
+//       const mp = new MP(link);
+//       await mp.scrape().then(() => {
+//         console.log(
+//           colors.green(`${mp.data.name} `) +
+//             `/ has been scrapped ` +
+//             colors.yellow(`${numberofMPsScraped++} `) +
+//             `of ` +
+//             colors.green(`${numberOfMPs}`),
+//         );
+//         mps.push(mp.data);
+//       });
+//     }
+//   }
+//   return mps;
+// }
+
+// function saveMembersInAFile(json) {
+//   fs.writeFile('./data/members.json', json, error => {
+//     if (error) {
+//       return console.error(colors.red(JSON.stringify(error)));
+//     }
+//     console.log(colors.bgWhite('The file was saved!'));
+//   });
+// }
 
 start();
--- src/mp.ts
+++ src/mp.ts
@@ -18,35 +18,52 @@ const getMp = async (url: string): Promise<MP> => {
   const page = await browser.newPage();
   await page.goto(url);
   const mp: MP = await page.evaluate(() => ({
-    name:
-      (document.querySelector('h1') as HTMLHeadingElement).innerText || null,
-    constituency:
-      (document.querySelector('#commons-constituency') as HTMLDivElement)
-        .innerText || null,
-    addressAs:
-      (document.querySelector('#commons-addressas') as HTMLDivElement)
-        .innerText || null,
-    party:
-      (document.querySelector('#commons-party') as HTMLDivElement).innerText ||
-      null,
-    email:
-      (document.querySelector(
-        'p[data-generic-id="email-address"] > a',
-      ) as HTMLAnchorElement).innerText || null,
+    name: (document.querySelector('h1') as HTMLHeadingElement)
+      ? (document.querySelector('h1') as HTMLHeadingElement).innerText
+      : null,
+    constituency: (document.querySelector(
+      '#commons-constituency',
+    ) as HTMLDivElement)
+      ? (document.querySelector('#commons-constituency') as HTMLDivElement)
+          .innerText
+      : null,
+    addressAs: (document.querySelector('#commons-addressas') as HTMLDivElement)
+      ? (document.querySelector('#commons-addressas') as HTMLDivElement)
+          .innerText
+      : null,
+    party: (document.querySelector('#commons-party') as HTMLDivElement)
+      ? (document.querySelector('#commons-party') as HTMLDivElement).innerText
+      : null,
+    email: (document.querySelector(
+      'p[data-generic-id="email-address"] > a',
+    ) as HTMLAnchorElement)
+      ? (document.querySelector(
+          'p[data-generic-id="email-address"] > a',
+        ) as HTMLAnchorElement).innerText
+      : null,
     twitter: {
-      handler:
-        (document.querySelector(
-          'li[data-generic-id="twitter"] > a',
-        ) as HTMLAnchorElement).innerText || null,
-      url:
-        (document.querySelector(
-          'li[data-generic-id="twitter"] > a',
-        ) as HTMLAnchorElement).href || null,
+      handler: (document.querySelector(
+        'li[data-generic-id="twitter"] > a',
+      ) as HTMLAnchorElement)
+        ? (document.querySelector(
+            'li[data-generic-id="twitter"] > a',
+          ) as HTMLAnchorElement).innerText
+        : null,
+      url: (document.querySelector(
+        'li[data-generic-id="twitter"] > a',
+      ) as HTMLAnchorElement)
+        ? (document.querySelector(
+            'li[data-generic-id="twitter"] > a',
+          ) as HTMLAnchorElement).href
+        : null,
     },
-    website:
-      (document.querySelector(
-        'li[data-generic-id="website"] > a',
-      ) as HTMLAnchorElement).href || null,
+    website: (document.querySelector(
+      'li[data-generic-id="website"] > a',
+    ) as HTMLAnchorElement)
+      ? (document.querySelector(
+          'li[data-generic-id="website"] > a',
+        ) as HTMLAnchorElement).href
+      : null,
   }));
   browser.close();
   return mp;
