94ceb2a06811681546119f7f93af4175d7b0c149
Pedro Martin <pataruco@gmail.com>
2019-03-05 19:51:45 +0000

    test start

--- index.test.ts
+++ index.test.ts
@@ -1,13 +1,28 @@
-import { scrapeMps } from './index';
-import * as mockGetMp from './src/mp';
 import { MP } from './typings/mp';
-// import getMp, { MP } from './src/mp';
-import colors from 'colors/safe';
+import * as mockGetMp from './src/mp';
+import * as mockGetMpIndex from './src/index';
+import start, { scrapeMps } from './index';
 
-jest.mock('./src/index');
-jest.mock('./src/mp');
 jest.mock('colors');
 console.log = jest.fn();
+
+const mockLinks = [
+  'https://www.parliament.uk/biographies/commons/nigel-adams/4057',
+];
+
+const mockMp: MP = {
+  addressAs: 'Nigel Adams',
+  constituency: 'Selby and Ainsty',
+  email: 'nigel.adams.mp@parliament.uk',
+  name: 'Nigel Adams MP',
+  party: 'Conservative',
+  twitter: {
+    handler: null,
+    url: null,
+  },
+  website: 'http://www.selbyandainsty.com/',
+};
+const mockMps = [mockMp];
 
 describe('scrapeMps', () => {
   afterEach(() => {
@@ -15,27 +30,26 @@ describe('scrapeMps', () => {
   });
 
   it('return an array of mps', async () => {
-    const mockLinks = [
-      'https://www.parliament.uk/biographies/commons/nigel-adams/4057',
-    ];
-
-    const mockMp: MP = {
-      addressAs: 'Nigel Adams',
-      constituency: 'Selby and Ainsty',
-      email: 'nigel.adams.mp@parliament.uk',
-      name: 'Nigel Adams MP',
-      party: 'Conservative',
-      twitter: {
-        handler: null,
-        url: null,
-      },
-      website: 'http://www.selbyandainsty.com/',
-    };
-
-    const mockMps = [mockMp];
     const getMpSpy = jest.spyOn(mockGetMp, 'default');
+
     getMpSpy.mockResolvedValueOnce(Promise.resolve(mockMp));
+
     const mps = await scrapeMps(mockLinks);
     expect(mps).toEqual(mockMps);
+  });
+});
+
+describe('start', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('start the scrapping process', async () => {
+    const getMpIndexSpy = jest.spyOn(mockGetMpIndex, 'default');
+    getMpIndexSpy.mockResolvedValueOnce(Promise.resolve(mockLinks));
+
+    await start();
+
+    expect(getMpIndexSpy).toBeCalled();
   });
 });
